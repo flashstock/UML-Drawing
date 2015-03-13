5324f32e3b5a5a062a2f4ea4b9b0f9f41ac22d3a
Johan Hugg <karljohanhugg@gmail.com>
2015-03-13 20:15:04 +0100

    More lambda expressions!!

--- src/hig/johanhugg/umldrawing/classloader/UMLClassLoader.java
+++ src/hig/johanhugg/umldrawing/classloader/UMLClassLoader.java
@@ -7,6 +7,7 @@ import hig.johanhugg.umldrawing.model.Visibility;
 
 import java.lang.reflect.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,33 +38,34 @@ public class UMLClassLoader {
 	public List<UMLAttribute> getMethods() {
 		if (loadedClass == null)
 			return null;
+
 		ArrayList<UMLAttribute> umlAttributes = new ArrayList<>();
-		Method[] methods = loadedClass.getDeclaredMethods();
-		for (int i = 0; i < methods.length; i++) {
-			Type[] typeParameters = methods[i].getGenericParameterTypes();
-			ArrayList<String> typeNames = new ArrayList<>();
 
-			for (int n = 0; n < typeParameters.length; n++)
-				typeNames.add(typeParameters[n].getTypeName());
+        Arrays.stream(loadedClass.getDeclaredMethods()).forEach(method -> {
+            ArrayList<String> typeNames = new ArrayList<>();
 
-			Class<?> returnType = methods[i].getReturnType();
+            Arrays.stream(method.getGenericParameterTypes()).forEach(type -> typeNames.add(type.getTypeName()));
 
-			umlAttributes.add(UMLAttributeFactory.createMethodAttribute(Visibility.getVisibilityFromModifier(methods[i].getModifiers()),
-					methods[i].getName(), typeNames.stream().map(Object::toString).collect(Collectors.joining(" ")), returnType.getName()));
+            Class<?> returnType = method.getReturnType();
 
-		}
+            umlAttributes.add(UMLAttributeFactory.createMethodAttribute(Visibility.getVisibilityFromModifier(method.getModifiers()),
+                    method.getName(), typeNames.stream().map(Object::toString).collect(Collectors.joining(" ")), returnType.getName()));
+        });
+
 		return umlAttributes;
 	}
 
 	public List<UMLAttribute> getFields() {
 		if (loadedClass == null)
 			return null;
+
 		ArrayList<UMLAttribute> umlFields = new ArrayList<>();
-		Field[] attributes = loadedClass.getDeclaredFields();
-		for (int i = 0; i < attributes.length; i++) {
-			umlFields.add(UMLAttributeFactory.createFieldAttribute(Visibility.getVisibilityFromModifier(attributes[i].getModifiers()),
-					attributes[i].getName(), attributes[i].getType().getSimpleName()));
-		}
+
+        Arrays.stream(loadedClass.getDeclaredFields()).forEach(attr -> {
+            umlFields.add(UMLAttributeFactory.createFieldAttribute(Visibility.getVisibilityFromModifier(attr.getModifiers()),
+                    attr.getName(), attr.getType().getSimpleName()));
+        });
+
 		return umlFields;
 
 	}
@@ -71,18 +73,19 @@ public class UMLClassLoader {
 	public List<UMLAttribute> getConstructors() {
 		if (loadedClass == null)
 			return null;
+
 		ArrayList<UMLAttribute> umlConstructors = new ArrayList<>();
-		Constructor[] constructors = loadedClass.getConstructors();
-		for (int i = 0; i < constructors.length; i++) {
-			Type[] typeParameters = constructors[i].getGenericParameterTypes();
-			ArrayList<String> typeNames = new ArrayList<>();
 
-			for (int n = 0; n < typeParameters.length; n++)
-				typeNames.add(typeParameters[n].getTypeName());
+        Arrays.stream(loadedClass.getConstructors()).forEach(cons -> {
+            Type[] typeParameters = cons.getGenericParameterTypes();
+            ArrayList<String> typeNames = new ArrayList<>();
 
-			umlConstructors.add(UMLAttributeFactory.createConstructorAttribute(Visibility.getVisibilityFromModifier(constructors[i].getModifiers()),
-					loadedClass.getSimpleName(), typeNames.stream().map(Object::toString).collect(Collectors.joining(" "))));
-		}
+            Arrays.stream(typeParameters).forEach(x -> typeNames.add(x.getTypeName()));
+
+            umlConstructors.add(UMLAttributeFactory.createConstructorAttribute(Visibility.getVisibilityFromModifier(cons.getModifiers()),
+                    loadedClass.getSimpleName(), typeNames.stream().map(Object::toString).collect(Collectors.joining(" "))));
+        });
+
 		return umlConstructors;
 	}
 
@@ -90,17 +93,19 @@ public class UMLClassLoader {
 		if (loadedClass == null)
 			return null;
 		UMLClass generatedClass = new UMLClass(loadedClass.getSimpleName());
+
+        getConstructors().forEach(x -> generatedClass.addAttribute(x));
 		getFields().forEach(x -> generatedClass.addAttribute(x));
 		getMethods().forEach(x -> generatedClass.addAttribute(x));
-		getConstructors().forEach(x -> generatedClass.addAttribute(x));
+
 		return generatedClass;
 	}
 
 	@Override
 	public String toString() {
 		List<UMLAttribute> allAttributes = new LinkedList<>();
-        allAttributes.addAll(getFields());
         allAttributes.addAll(getConstructors());
+        allAttributes.addAll(getFields());
         allAttributes.addAll(getMethods());
 
         return allAttributes.stream().map(Object::toString).collect(Collectors.joining("\n"));

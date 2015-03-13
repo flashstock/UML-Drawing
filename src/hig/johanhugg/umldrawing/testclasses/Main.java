331995dd1394ed26ebfd1b28917f77e03db1d611
Johan Hugg <karljohanhugg@gmail.com>
2015-03-13 12:31:10 +0100

    Added testclass

---
+++ src/hig/johanhugg/umldrawing/testclasses/Main.java
@@ -0,0 +1,22 @@
+package hig.johanhugg.umldrawing.testclasses;
+
+public class Main {
+	private int x = 10;
+	public int y = 20;
+	protected int bla = 30;
+	protected int xD = 50;
+	public Main(int x) {
+		xD = x;
+	}
+	
+	private static final String hello = "Hello there!";
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	private int subtract(int x, int y) {
+		return x - y;
+	}
+
+
+}
